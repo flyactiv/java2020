39b27484567040e8020fab7e8f23dc9fa5846569
Alexandra <70800450+flyactiv@users.noreply.github.com>
2020-11-09 15:15:30 +0300

    Add files via upload

---
+++ lab18/Maximum.java
@@ -0,0 +1,18 @@
+package ru.mirea.lab18;
+
+public class Maximum {
+    public static int recursion() {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int n = in.nextInt();
+        // Базовый случай
+        if (n == 0) {
+            return 0;
+        } // Шаг рекурсии / рекурсивное условие
+        else {
+            return Math.max(n, recursion());
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(recursion()); // вызов рекурсивной функции
+    }
+}
